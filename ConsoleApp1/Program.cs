00d6a769a8518de17011e7ff389968b9ad220296
atthy4 <74587739+atthy4@users.noreply.github.com>
2021-03-09 11:20:40 +0100

    asd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+            NewMethod(arr);
+            Console.Read();
+        }
+        public static int[] NewMethod(int[] arr)
+        {
+            int n = arr.Length;
+            Console.WriteLine("Selection sort");
+            Console.Write("Initial array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            int temp, smallest;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine();
+            Console.Write("Sorted array is: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            return arr;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void NewMethodTest()
+        {
+            //Arrange
+            int[] tomb = new int[] { 3, 2, 1 };
+            //Act
+            int[] rendezett = Program.NewMethod(tomb);
+            //Assert
+            Assert.AreEqual(rendezett[0], 1);
+        }
+
+        [TestMethod()]
+        public void NewMethodTest2()
+        {
+            //arrange
+            int[] tomb = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            //act
+            int[] rendezett = Program.NewMethod(tomb);
+            //assert
+            for (int i = 0; i < rendezett.Length; i++)
+            {
+                if (i + 1 < rendezett.Length)
+                {
+                    Assert.IsTrue(rendezett[i] < rendezett[i + 1]);
+                }
+            }
+        }
+    }
+}
