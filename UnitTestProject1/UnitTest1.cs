6830aceab2156dfc3fca945f722f4b45e25c4444
atthy4 <74587739+atthy4@users.noreply.github.com>
2021-03-09 12:47:29 +0100

    asd
    asd

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -43,7 +43,7 @@ namespace UnitTestProject1
             dw.Manage().Window.Maximize();
             dw.Navigate().GoToUrl("https://www.portfolio.hu/");
             string s = dw.Url;
-            Assert.AreEqual(s, "asd");
+            Assert.AreEqual(s, "https://www.portfolio.hu/");
         }
     }
 }
